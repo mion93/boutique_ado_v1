9651fc74fe3d60c855a7b5a2ee56154e32bfb9a9
Miriam Gurrieri <mion93@hotmail.it>
2021-01-13 12:26:52 +0000

    added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51I97ngBd31GojJZkCtlyhNS5zMrITtBHLM8XTeO6tWxdR256yvaqCyc3PYFQuFotDkuvMtR9m7p9e2RgWlMoTAqe00wQzgRpmx',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
